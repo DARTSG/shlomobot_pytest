3aa8ed17e120b3d283242bd054cf38cc63e9212d
Achmad Ridzwan <achmad@dart.com.sg>
2022-07-25 16:49:29 +0800

    functions to return namely one variable instead of multiple variables in dict/Users/achmad/.pyenv/versions/3.9.7/bin/python

--- shlomobot_pytest/common_tests.py
+++ shlomobot_pytest/common_tests.py
@@ -1,21 +1,19 @@
 from importlib import import_module
 
-from shlomobot_pytest.utils import find_functions
+from shlomobot_pytest.utils import find_functions, extract_functions_in_order
 import inspect
 import re
-from typing import Dict, List, Union
+from typing import Dict, List
 
 
-def check_name_main_statement(
-    file_function_dict: Dict[str, List[str]]
-) -> Dict[str, Union[bool, List]]:
+def check_name_main_statement(file_function_dict: Dict[str, List[str]]) -> int:
     """
     Check if the "if __name__ == '__main__'" statement is present
 
-    Returns a dict consisting of error and error count 1 if the statement is absent in all files
+    Returns a variable 'error count' 0 or 1; 1 if the statement is absent in all files
     """
     statement_present_files = []
-    results = {"error": True, "error_count": 0}
+    error_count = 0
     for filename in file_function_dict.keys():
         file = filename.rstrip(".py")
         user_file = import_module(file)
@@ -29,45 +27,35 @@ def check_name_main_statement(
         if name_main_line:
             statement_present_files.append(filename)
     if not statement_present_files:
-        results["error"] = False
-        results["error_count"] = 1
-    return results
+        error_count = 1
+    return error_count
 
 
-def check_main_func_order(
-    file_function_dict: Dict[str, List[str]]
-) -> Dict[str, Union[bool, List]]:
+def check_main_func_order(file_function_dict: Dict[str, List[str]]) -> List:
     """
     Check if the 'main' function is the last function
 
-    Returns a dict consisting of error and list of filenames where main func is not the last
+    Returns a list of filenames where main func is present but not the last
     """
     filenames = []
-    results = {"error": True, "filenames": filenames}
     for filename in file_function_dict.keys():
-        file = filename.rstrip(".py")
-        user_file = import_module(file)
-        functions = find_functions(user_file)
-        if functions[-1] != "main":
-            filenames.append(filename)
-    if filenames:
-        results["error"] = False
-    return results
+        functions = extract_functions_in_order(filename)
+
+        # Check if file contains 'main' function and if it is last
+        for function in functions:
+            if function == "main" and functions[-1] != function:
+                filenames.append(filename)
+
+    return filenames
 
 
-def check_missing_docstrings(
-    file_function_dict: Dict[str, List[str]]
-) -> Dict[str, Union[bool, List]]:
+def check_missing_docstrings(file_function_dict: Dict[str, List[str]]) -> List:
     """
     Check if the user created docstrings for all functions except 'main' function
 
-    Returns a dict consisting of error and list of functions where docstrings are missing
+    Returns a list of functions where docstrings are missing
     """
     func_missing_docstrings = []
-    results = {
-        "error": True,
-        "missing_docstrings": func_missing_docstrings,
-    }
     for filename in file_function_dict.keys():
         file = filename.rstrip(".py")
         user_file = import_module(file)
@@ -76,29 +64,21 @@ def check_missing_docstrings(
         for function in functions:
             # Skip checking docstrings for 'main' function
             if function.__name__ == "main":
-                pass
+                continue
             # Handle missing docstrings
-            elif not function.__doc__:
+            if not function.__doc__:
                 func_missing_docstrings.append(function.__name__)
 
-    if func_missing_docstrings:
-        results["error"] = False
-    return results
+    return func_missing_docstrings
 
 
-def check_single_quote_docstrings(
-    file_function_dict: Dict[str, List[str]]
-) -> Dict[str, Union[bool, List]]:
+def check_single_quote_docstrings(file_function_dict: Dict[str, List[str]]) -> List:
     """
     Check if the user created docstrings using double or single quotes
 
-    Returns a dict consisting of error and list of functions where single quotes were used for docstrings
+    Returns a list of functions where single quotes were used for docstrings
     """
     single_quote_docstrings = []
-    results = {
-        "error": True,
-        "single_quote_docstr": single_quote_docstrings,
-    }
     for filename in file_function_dict.keys():
         file = filename.rstrip(".py")
         user_file = import_module(file)
@@ -109,27 +89,23 @@ def check_single_quote_docstrings(
             if function.__name__ == "main":
                 pass
             # Handle double quotes check if docstrings exist
-            elif function.__doc__:
+            if function.__doc__:
                 func_code = inspect.getsource(function)
                 dbl_quotes_docstring = re.search(r"\"\"\"[\s\S]*?\"\"\"", func_code)
                 if not dbl_quotes_docstring:
                     single_quote_docstrings.append(function.__name__)
 
-    if single_quote_docstrings:
-        results["error"] = False
-    return results
+    return single_quote_docstrings
 
 
-def check_main_function(
-    file_function_dict: Dict[str, List[str]]
-) -> Dict[str, Union[bool, List]]:
+def check_main_function(file_function_dict: Dict[str, List[str]]) -> int:
     """
     Check if the user created a 'main' function
 
-    Returns a dict consisting of error and error count 1 if main function is absent in all files
+    Returns a variable 'error_count' 0 or 1; 1 if main function is absent in all files
     """
     main_func_present = []
-    results = {"error": True, "error_count": 0}
+    error_count = 0
     for filename in file_function_dict.keys():
         try:
             file = filename.rstrip(".py")
@@ -139,6 +115,9 @@ def check_main_function(
         except AttributeError:
             pass
     if not main_func_present:
-        results["error"] = False
-        results["error_count"] = 1
-    return results
+        error_count = 1
+
+    return error_count
+
+
+print(check_main_func_order({"pretest.py": []}))
