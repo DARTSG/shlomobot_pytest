7fddd17805956f71cde5ab243c4b9594a13e32cd
Achmad Ridzwan <achmad@dart.com.sg>
2022-08-08 18:48:56 +0800

    changed assertion functions to receive custom points per error and max points per error where applicable

--- shlomobot_pytest/assertions.py
+++ shlomobot_pytest/assertions.py
@@ -7,37 +7,49 @@ from shlomobot_pytest.pretest import (
 from shlomobot_pytest.utils import create_custom_error_json
 
 
-def assert_missing_expected_files(filenames_list: list[str]):
+def assert_missing_expected_files(
+    filenames_list: list[str],
+    points_per_error: int = 100,
+    max_points_deducted: int = 100,
+):
     """Assert that no expected files are missing; If missing, raise a custom error message"""
 
     wrongly_named_files = find_missing_expected_files(filenames_list)
 
     custom_error_message = create_custom_error_json(
+        points_per_error,
+        max_points_deducted,
         feedback=f"The name for the following submitted file(s) are wrong: {', '.join(wrongly_named_files)}",
-        points_per_error=100,
-        max_points_deducted=100,
         number_of_errors=len(wrongly_named_files),
     )
 
     assert wrongly_named_files == [], custom_error_message
 
 
-def assert_missing_expected_functions(file_function_map: dict[str, list[str]]):
+def assert_missing_expected_functions(
+    file_function_map: dict[str, list[str]],
+    points_per_error: int = 100,
+    max_points_deducted: int = 100,
+):
     """Assert that no expected functions are missing; If missing, raise a custom error message"""
 
     wrongly_named_functions = find_missing_expected_functions(file_function_map)
 
     custom_error_message = create_custom_error_json(
+        points_per_error,
+        max_points_deducted,
         feedback=f"The name for the following function(s) are wrong: {', '.join(wrongly_named_functions)}",
-        points_per_error=100,
-        max_points_deducted=100,
         number_of_errors=len(wrongly_named_functions),
     )
 
     assert wrongly_named_functions == [], custom_error_message
 
 
-def assert_pep8_conformance(filenames_list: list[str]):
+def assert_pep8_conformance(
+    filenames_list: list[str],
+    points_per_error: int = 5,
+    max_points_deducted: int = 30,
+):
     """Assert that Pep8 is conformed to; If not, raise a custom error message with the errors"""
     pep8_errors = pep8_conformance(filenames_list)
 
@@ -49,10 +61,10 @@ def assert_pep8_conformance(filenames_list: list[str]):
         total_num_of_errors += len(error_messages_list)
 
     custom_error_message = create_custom_error_json(
+        points_per_error,
+        max_points_deducted,
         total_num_of_errors,
         feedback=f"{'; '.join(combined_errors_string_list)}",
-        points_per_error=5,
-        max_points_deducted=30,
     )
 
     assert pep8_errors == {}, custom_error_message
