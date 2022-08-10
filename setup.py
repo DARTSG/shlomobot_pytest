54ce5e7f3b7028fb2ee85b7112509bba238e95cf
Achmad Ridzwan <achmad@dart.com.sg>
2022-08-10 14:06:52 +0800

    update library version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 NAME = "shlomobot_pytest"
-VERSION = "1.0.0"
+VERSION = "2.0.0"
 # To install the library, run the following
 #
 # python setup.py install
