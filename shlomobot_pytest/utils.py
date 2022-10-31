7914341bd01f67f4697bdd9d832a1b6c218f83e1
Dvir Horovitz <dvirh@dart.com.sg>
2022-10-31 15:06:18 +0800

    Added a function that simulates input-output of given script

--- shlomobot_pytest/utils.py
+++ shlomobot_pytest/utils.py
@@ -1,6 +1,32 @@
 import inspect
 import re
 from types import ModuleType, FunctionType
+import pytest
+import sys
+from io import StringIO
+
+@pytest.fixture()
+def simulate_input_output(monkeypatch, capsys: pytest.CaptureFixture):
+    """
+    A fixture to simulate input-output of a python file.
+    This should be called with the filename parameter, and than the input(s) as *args.
+    Each given argument is equivalent to 1 line of input (splitted by \\n)
+    """
+
+    def _internal(filename="", *args):
+        send_input_string = ""
+        
+        # Converting args to list, to maintain the order of the recived inputs.
+        for item in args:
+            send_input_string += str(item) + "\n"
+        monkeypatch.setattr(sys, 'stdin', StringIO(send_input_string))
+
+        with open(filename, 'r') as f:
+            exec(f.read())
+        user_output = capsys.readouterr().out
+        return user_output
+
+    return _internal
 
 
 def extract_functions(module: ModuleType) -> list[FunctionType]:
