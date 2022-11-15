d132b10fb09f21817f3917c1910b6b513af1036d
ronpeer <ron_peer@dart.com.sg>
2022-11-15 15:05:11 +0800

    up to date

--- shlomobot_pytest/common_tests.py
+++ shlomobot_pytest/common_tests.py
@@ -39,7 +39,6 @@ def is_main_function_last(module_name: str) -> bool:
 def find_functions_with_missing_docstrings(file_list: list[str]) -> list[str]:
     """
     Checks if the user created docstrings for all functions except 'main' function
-
     Returns a list of functions where docstrings are missing
     """
 
@@ -60,7 +59,6 @@ def find_functions_with_missing_docstrings(file_list: list[str]) -> list[str]:
 def find_functions_with_single_quote_docstrings(file_list: list[str]) -> list[str]:
     """
     Check if the user created docstrings using double or single quotes
-
     Returns a list of functions where single quotes were used for docstrings
     """
     single_quote_docstrings = []
@@ -90,3 +88,68 @@ def contains_main_function(module_name: str) -> bool:
         return True
     except AttributeError:
         return False
+
+
+def builtins_not_used_as_variable(file_list: list[str]) -> bool:
+    """
+    Checks if inside the files given in 'file_list' any builtins are used as variables
+    Return True if no builtins are used as variables, False if at least one is.
+    """
+    builtins_list = [word for word in dir(builtins) if not re.match("[A-Z|_]", word[0])]
+    functions_list = get_functions_from_files(file_list)
+
+    builtin_used_as_variable = r"\n\s*(?:[^\s]*? ?, ?)*?{0} ?(?:\s*,\s*[^\W]+?\s*)*=.*"
+    builtin_given_to_function = r"def {0}\((?:[^\s]*? ?, ?)*?{1}(?:\s*,\s*[^\W]+?)*\):"
+
+    for function_name, function in functions_list:
+        # extract function code
+        function_code = inspect.getsource(function)
+        # look for all possible builtin words used in the function
+        for builtin_word in builtins_list:
+            builtin_as_variable_regex = builtin_used_as_variable.format(builtin_word)
+            builtin_as_paramater_regex = builtin_given_to_function.format(
+                function_name, builtin_word
+            )
+            occurences_as_variable = re.search(builtin_as_variable_regex, function_code)
+            occurences_as_paramater = re.search(
+                builtin_as_paramater_regex, function_code
+            )
+            if occurences_as_variable or occurences_as_paramater:
+                return False
+
+    return True
+
+
+def declared_global_variable(file_list: list[str]) -> bool:
+    """
+    checks if in the code of the files inside file_list there was decleration of a global variable
+    """
+    global_decleration_regex = re.compile(
+        r"\n\s*global\s+[^\W]+(?:\s*,\s*[^\W]+\s*)*\n"
+    )
+
+    functions_list = get_functions_from_files(file_list)
+
+    for function in functions_list:
+        # function is a tuple of the function name and object, but in this case only the object is needed
+        function_code = inspect.getsource(function[1])
+        if re.search(global_decleration_regex, function_code):
+            return True
+
+    return False
+
+
+def contains_with_open(module_name: str, function_name_list: list[str]) -> bool:
+    """checks for loops in the functions in function_name list, inside of the given module"""
+    
+    functions_list = get_functions_from_files(module_name)
+
+    with_open_regex_pattern = r"with\s+open\(['\"]"
+
+    for function_name, function in functions_list:
+        if function_name in function_name_list and not re.search(
+            with_open_regex_pattern, inspect.getsource(function)
+        ):
+            return False
+
+    return True
